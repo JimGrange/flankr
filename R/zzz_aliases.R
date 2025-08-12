51535dc176bfc79bbe687dd22c218cb6cab078af
Jim Grange <grange.jim@gmail.com>
2025-08-12 03:51:07 +0100

    added SSP aliases

--- R/zzz_aliases.R
+++ R/zzz_aliases.R
@@ -23,3 +23,31 @@ fit_dstp_fixed <- fitDSTP_fixed
 #' @rdname fitMultipleDSTP_fixed
 #' @export
 fit_multiple_dstp_fixed <- fitMultipleDSTP_fixed
+
+
+
+
+# snake case SSP ----------------------------------------------------------
+#' @rdname simulateSSP
+#' @export
+simulate_ssp <- simulateSSP
+
+#' @rdname fitSSP
+#' @export
+fit_ssp <- fitSSP
+
+#' @rdname fitMultipleSSP
+#' @export
+fit_multiple_ssp <- fitMultipleSSP
+
+#' @rdname fitMultipleSSP
+#' @export
+fit_multiple_ssp <- fitMultipleSSP
+
+#' @rdname fitSSP_fixed
+#' @export
+fit_ssp_fixed <- fitSSP_fixed
+
+#' @rdname fitMultipleSSP_fixed
+#' @export
+fit_multiple_ssp_fixed <- fitMultipleSSP_fixed
